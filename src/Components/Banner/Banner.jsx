28139bcb1a8571a2cbd9a19c1a699ef1b115b32e
yeasinChowdhury <depressedman996@gmail.com>
2025-05-27 21:02:57 +0600

    add new change

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
      <div>
       <Container>
         <div className='text-center'>
-          <h1 className=' w-[560px] font-Secoundariy text-[56px] m-auto leading-[64px]'>Get your work done with  Management Tool </h1>
+          <h1 className='w-auto md:w-[560px] font-Secoundariy text-[30px] md:text-[56px] m-auto leading-[40px] md:leading-[64px]'>Get your work done with  Management Tool </h1>
           <p className='w-[467px] font-Secoundariy text-lg m-auto leading-8 mt-5'>The world's first project management platform that connects everything</p>
           <div className='relative mt-8'>
             <input type="text" className='w-[495px] py-5 pl-6 bg-white rounded-[6px] placeholder:text-gray-500 text-gray-500 outline-0' placeholder='Your business email' />
--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../assets/logo (3).png'
-import Container from '../Layout/Container'
+import Container from '../Layout/Container';
+import { FaListCheck } from "react-icons/fa6";
+import { ImCross } from "react-icons/im";
+
 
 
 const Navber = () => {
+  const [show, setShow] =useState(false)
+  const handleClick = ()=>{
+    setShow(true)
+    
+  }
   return (
     <div className='pt-[32px] absolute top-0 left-0 w-full'>
       <Container>
-        <div className='flex justify-between items-center'>
-          <div>
+        <div className='md:flex justify-between items-center'>
+          <div className='flex  items-center justify-between '>
             <img src={logo} alt="wsr" />
+            <div>
+              <FaListCheck onClick={handleClick} className='block md:hidden text-2xl text-white' />
+            </div>
           </div>
           <div>
-            <ul className='flex gap-x-[50px] font-Secoundariy text-lg font-medium text-white'>
+            <ul className=' hidden md:flex gap-x-[50px] font-Secoundariy text-lg font-medium text-white'>
               <li>Home </li>
               <li>Features </li>
               <li>Service </li>
@@ -20,14 +31,46 @@ const Navber = () => {
               <li>Blog </li>
             </ul>
           </div>
-          <div className='flex items-center gap-x-9 '>
+          <div className=' hidden md:flex items-center gap-x-9 '>
             <p className='font-Secoundariy text-lg font-medium text-white'>Login</p>
-            <a className='font-Secoundariy text-lg font-medium py-5 px-12 bg-secoundry rounded-[6px]'>Register</a>
+           <div>
+             <a className='font-Secoundariy text-lg font-medium py-5 px-12 bg-secoundry rounded-[6px] inline-block'>Register</a>
+           </div>
 
           </div>
         </div>
       </Container>
 
+    {
+      show &&
+     <div className='bg-blue-300 w-full h-screen absolute top-0 left-0 flex justify-center items-center z-[999] text-center'>
+      
+      <div>
+        <ImCross onClick={()=>setShow(false)} className='text-2xl text-white' />
+      </div>
+        <div>
+            <ul className=' gap-x-[50px] font-Secoundariy text-lg font-medium text-white'>
+              <li className='mt-5'>Home </li>
+              <li className='mt-5'>Features </li>
+              <li className='mt-5'>Service </li>
+              <li className='mt-5'>Pages </li>
+              <li>Blog </li>
+            </ul>
+            <div className=' gap-x-9 '>
+            <p className='font-Secoundariy text-lg font-medium text-white mt-5'>Login</p>
+           <div className='mt-5'>
+             <a className='font-Secoundariy text-lg font-medium py-5 px-12 bg-secoundry rounded-[6px] inline-block'>Register</a>
+           </div>
+
+          </div>
+          </div>
+          
+
+
+     </div>
+
+     
+    }
 
     </div>
   )
